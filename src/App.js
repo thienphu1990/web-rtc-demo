5edeb9ade995873e8846d8e0384ec459bbb123b9
Trinh Phu <phutrinh@Trinhs-MacBook-Pro.local>
2020-07-18 11:43:46 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,410 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import styled from 'styled-components'
+import adapter from 'webrtc-adapter';
+import * as firebase from 'firebase';
+import uid from 'uid'
 
-function App() {
+const Bound = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  .video-container{
+    display:flex;
+    position: relative;
+    width:50%;
+    left: 50%;
+    transform: translate(-50%, 0);
+    #local-video{
+      /* max-width: 100%; */
+      width: 100%;
+      z-index:1;
+    }
+    #remote-video{
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      /* max-width: 20%; */
+      width: 30%;
+      z-index: 0;
+    }
+  }
+  .button-container{
+    display: flex;
+    flex-direction: row;
+    width:100%;
+    height: 50px;
+    margin: 20px 0;
+    justify-content:center;
+    #start, #call, #hangup{
+      width: 150px;
+      height:50px;
+      border-radius: 5px;
+      background-color: #33ADFF;
+      font-weight:bold;
+      font-size: 15px;
+      color: #fff;
+      display:flex;
+      align-items:center;
+      justify-content:center;
+      margin: 0 10px;
+      :disabled{
+        background-color: #cccccc;
+      }
+    }
+  }
+  
+`
+const mediaStreamConstraints = {
+  video: {
+    width: {
+      min: 640,
+      max: 1280
+    },
+    height: {
+      min: 480,
+      max: 720
+    }
+  },
+  audio: true
+}
+
+var fb = firebase.initializeApp({ 
+  apiKey: "AIzaSyB-q7FU6fQQTOShT_UbybVUBOcXHAhTILo",
+  authDomain: "vinpearl-2f7d4.firebaseapp.com",
+  databaseURL: "https://vinpearl-2f7d4.firebaseio.com",
+  projectId: "vinpearl-2f7d4",
+  storageBucket: "vinpearl-2f7d4.appspot.com",
+  messagingSenderId: "904958325116",
+  appId: "1:904958325116:web:e123ec52c8eea170"
+});
+const node = 'vinhome/videocall/web-rtc'
+var firebaseDB = fb.database();
+
+const offerOptions = {
+  offerToReceiveVideo: 1,
+}
+
+let localVideo;
+let remoteVideo;
+
+let callButton;
+let startButton;
+let hangupButton;
+
+let startTime = null;
+
+let localStream;
+let remoteStream;
+
+let localPeerConnection;
+let remotePeerConnection;
+
+const id = uid(10)
+console.log(id)
+
+const servers = {iceServers: [
+  {'urls': 'stun:stun.l.google.com:19302'},
+  {'urls': 'stun:stun.services.mozilla.com'},
+]};
+
+const App = () => {
+
+  useEffect(() => {
+    localVideo = document.getElementById('local-video')
+    remoteVideo = document.getElementById('remote-video')
+    startButton = document.getElementById('start')
+    callButton = document.getElementById('call')
+    hangupButton = document.getElementById('hangup');
+
+    callButton.disabled = true
+    hangupButton.disabled = true
+
+    localVideo.addEventListener('loadedmetadata', logVideoLoaded)
+    remoteVideo.addEventListener('loadedmetadata', logVideoLoaded)
+    remoteVideo.addEventListener('onresize', logResizedVideo)
+
+    return () => {
+      localVideo.removeEventListener('loadedmetadata', logVideoLoaded)
+      remoteVideo.removeEventListener('loadedmetadata', logVideoLoaded)
+      remoteVideo.removeEventListener('onresize', logResizedVideo)
+    }
+  }, [])
+
+  const watchFirebaseChange = () => {
+    firebaseDB.ref(node+'/web-rtc').on('value',(snapshot)=>{
+      let val = snapshot.val()
+      // readMessage(val)
+    })
+  }
+  
+  const gotLocalMediaStream = (mediaStream) => {
+    if(!localVideo) return
+    localVideo.srcObject = mediaStream
+    localStream = mediaStream
+    trace('Received local stream.')
+    callButton.disabled = false
+  }
+
+  const handleLocalMediaStreamError = (error) => {
+    trace(`navigator.getUserMedia error: ${error.toString()}.`)
+  }
+  
+  const gotRemoteMediaStream = (event) => {
+    const mediaStream = event.stream
+    remoteVideo.style.zIndex = 2
+    remoteVideo.srcObject = mediaStream
+    remoteStream = mediaStream
+    trace('Remote peer connection received remote stream.')
+  }
+
+  const logVideoLoaded = (event) => {
+    const video = event.target
+    trace(`${video.id} videoWidth: ${video.videoWidth}px, ` +
+          `videoHeight: ${video.videoHeight}px.`)
+  }
+
+  const logResizedVideo = (event) => {
+    logVideoLoaded(event)
+    if (startTime) {
+      const elapsedTime = window.performance.now() - startTime
+      startTime = null
+      trace(`Setup time: ${elapsedTime.toFixed(3)}ms.`)
+    }
+  }
+
+// Define RTC peer connection behavior.
+
+  // Connects with new peer candidate.
+  const handleConnection = (event) => {
+    const peerConnection = event.target;
+    const iceCandidate = event.candidate;
+
+    if (iceCandidate) {
+      const newIceCandidate = new RTCIceCandidate(iceCandidate);
+      const otherPeer = getOtherPeer(peerConnection);
+
+      otherPeer.addIceCandidate(newIceCandidate)
+        .then(() => {
+          handleConnectionSuccess(peerConnection);
+        }).catch((error) => {
+          handleConnectionFailure(peerConnection, error);
+        });
+
+      trace(`${getPeerName(peerConnection)} ICE candidate:\n` +
+            `${event.candidate.candidate}.`);
+    }
+  }
+
+  // Logs that the connection succeeded.
+  const handleConnectionSuccess = (peerConnection) => {
+    trace(`${getPeerName(peerConnection)} addIceCandidate success.`);
+  };
+
+  // Logs that the connection failed.
+  const handleConnectionFailure = (peerConnection, error) => {
+    trace(`${getPeerName(peerConnection)} failed to add ICE Candidate:\n`+
+          `${error.toString()}.`);
+  }
+
+  // Logs changes to the connection state.
+  const handleConnectionChange = (event) => {
+    const peerConnection = event.target;
+    console.log('ICE state change event: ', event);
+    trace(`${getPeerName(peerConnection)} ICE state: ` +
+          `${peerConnection.iceConnectionState}.`);
+  }
+
+  // Logs error when setting session description fails.
+  const setSessionDescriptionError = (error) => {
+    trace(`Failed to create session description: ${error.toString()}.`);
+  }
+
+  // Logs success when setting session description.
+  const setDescriptionSuccess = (peerConnection, functionName) => {
+    const peerName = getPeerName(peerConnection);
+    trace(`${peerName} ${functionName} complete.`);
+  }
+
+  // Logs success when localDescription is set.
+  const setLocalDescriptionSuccess = (peerConnection) => {
+    setDescriptionSuccess(peerConnection, 'setLocalDescription');
+  }
+
+  // Logs success when remoteDescription is set.
+  const setRemoteDescriptionSuccess = (peerConnection) => {
+    setDescriptionSuccess(peerConnection, 'setRemoteDescription');
+  }
+
+  // Logs offer creation and sets peer connection session descriptions.
+  const createdOffer = (description) => {
+    trace(`Offer from localPeerConnection:\n${description.sdp}`);
+
+    trace('localPeerConnection setLocalDescription start.');
+    localPeerConnection.setLocalDescription(description)
+      .then(() => {
+        setLocalDescriptionSuccess(localPeerConnection);
+        // sendMessage(id, JSON.stringify({'sdp': localPeerConnection.localDescription}))
+      }).catch(setSessionDescriptionError);
+
+    trace('remotePeerConnection setRemoteDescription start.');
+    remotePeerConnection.setRemoteDescription(description)
+      .then(() => {
+        setRemoteDescriptionSuccess(remotePeerConnection);
+      }).catch(setSessionDescriptionError);
+
+    trace('remotePeerConnection createAnswer start.');
+    remotePeerConnection.createAnswer()
+      .then(createdAnswer)
+      .catch(setSessionDescriptionError);
+  }
+
+  // Logs answer to offer creation and sets peer connection session descriptions.
+  const createdAnswer = (description) => {
+    trace(`Answer from remotePeerConnection:\n${description.sdp}.`);
+
+    trace('remotePeerConnection setLocalDescription start.');
+    remotePeerConnection.setLocalDescription(description)
+      .then(() => {
+        setLocalDescriptionSuccess(remotePeerConnection);
+      }).catch(setSessionDescriptionError);
+
+    trace('localPeerConnection setRemoteDescription start.');
+    localPeerConnection.setRemoteDescription(description)
+      .then(() => {
+        setRemoteDescriptionSuccess(localPeerConnection);
+      }).catch(setSessionDescriptionError);
+  }
+  
+// Define and add behavior to buttons.
+
+  // Handles start button action: creates local MediaStream.
+  const startAction = () => {
+    startButton.disabled = true;
+    navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+      .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+    trace('Requesting local stream.');
+  }
+
+  // Handles call button action: creates peer connection.
+  const callAction = () => {
+    callButton.disabled = true;
+    hangupButton.disabled = false;
+
+    trace('Starting call.');
+    startTime = window.performance.now();
+    // Watch firebase
+    watchFirebaseChange()
+    // Get local media stream tracks.
+    const videoTracks = localStream.getVideoTracks();
+    const audioTracks = localStream.getAudioTracks();
+    if (videoTracks.length > 0) {
+      trace(`Using video device: ${videoTracks[0].label}.`);
+    }
+    if (audioTracks.length > 0) {
+      trace(`Using audio device: ${audioTracks[0].label}.`);
+    }
+
+    // Create peer connections and add behavior.
+    localPeerConnection = new RTCPeerConnection(servers);
+    trace('Created local peer connection object localPeerConnection.');
+
+    localPeerConnection.addEventListener('icecandidate', handleConnection);
+    localPeerConnection.addEventListener(
+      'iceconnectionstatechange', handleConnectionChange);
+
+    remotePeerConnection = new RTCPeerConnection(servers);
+    trace('Created remote peer connection object remotePeerConnection.');
+
+    remotePeerConnection.addEventListener('icecandidate', handleConnection);
+    remotePeerConnection.addEventListener(
+      'iceconnectionstatechange', handleConnectionChange);
+    remotePeerConnection.addEventListener('addstream', gotRemoteMediaStream);
+
+    // Add local stream to connection and create offer to connect.
+    localPeerConnection.addStream(localStream);
+    trace('Added local stream to localPeerConnection.');
+
+    trace('localPeerConnection createOffer start.');
+    localPeerConnection.createOffer(offerOptions)
+      .then(createdOffer).catch(setSessionDescriptionError);
+  }
+
+  // Handles hangup action: ends up call, closes connections and resets peers.
+  const hangupAction = () => {
+    localPeerConnection.close();
+    remotePeerConnection.close();
+    localPeerConnection = null;
+    remotePeerConnection = null;
+    hangupButton.disabled = true;
+    callButton.disabled = false;
+    remoteVideo.style.zIndex = 0;
+    trace('Ending call.');
+  }
+
+// Define helper functions.
+
+  // Gets the "other" peer connection.
+  const getOtherPeer = (peerConnection) => {
+    return (peerConnection === localPeerConnection) ?
+        remotePeerConnection : localPeerConnection;
+  }
+
+  // Gets the name of a certain peer connection.
+  const getPeerName = (peerConnection) => {
+    return (peerConnection === localPeerConnection) ?
+        'localPeerConnection' : 'remotePeerConnection';
+  }
+
+  const trace = (text) => {
+    text = text.trim();
+    const now = (window.performance.now() / 1000).toFixed(3);
+    console.log(now, text);
+  }
+
+  const sendMessage = (senderId, data) => {
+    firebaseDB.ref(node).set({ sender: senderId, message: data })
+  }
+
+  const readMessage = (data) => {
+    if(!data) return
+    var msg = JSON.parse(data.message);
+    var sender = data.sender;
+    if (sender !== id) {
+      if (msg.ice !== undefined) {
+        localPeerConnection.addIceCandidate(new RTCIceCandidate(msg.ice));
+      }
+      else if (msg.sdp.type === "offer") {
+        console.log('read message offer')
+        localPeerConnection.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+          .then(() => localPeerConnection.createAnswer())
+          .then(answer => localPeerConnection.setLocalDescription(answer))
+          .then(() => sendMessage(id, JSON.stringify({'sdp': localPeerConnection.localDescription})))
+          .catch(error => {
+            console.error( error);
+          });
+      }
+      else if (msg.sdp.type === "answer") {
+        console.log('read message answer')
+        localPeerConnection.setRemoteDescription(new RTCSessionDescription(msg.sdp)).catch(error => {
+          console.error( error);
+        });
+      }
+    }
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Bound>
+      <div className='button-container'>
+        <button id="start" onClick={()=>startAction()}>Start</button>
+        <button id="call" onClick={()=>callAction()}>Call</button>
+        <button id="hangup" onClick={()=>hangupAction()}>Hang Up</button>
+      </div>
+      <div className='video-container'>
+        <video id="local-video" autoPlay></video>
+        <video id="remote-video" autoPlay></video>
+      </div>
+    </Bound>
   );
 }
 
---
+++ src/Appv2.js
@@ -0,0 +1,351 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components'
+// import Peer from 'peerjs';
+import uid from 'uid'
+import * as firebase from 'firebase';
+import { findAllByDisplayValue } from '@testing-library/react';
+
+const Bound = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  input{
+    text-align: center;
+    margin: 8px 0;
+  }
+  .video-container{
+    display:flex;
+    position: relative;
+    width:50%;
+    #local-video{
+      position: absolute;
+      top: 0; 
+      right: 0;
+      /* max-width: 100%; */
+      width: 100%;
+      z-index:1;
+    }
+    #remote-video{
+      position: absolute;
+      top: 0;
+      right: 0;
+      /* max-width: 20%; */
+      width: 100%;
+      z-index: 0;
+    }
+  }
+  .button-container{
+    display: flex;
+    flex-direction: row;
+    width:100%;
+    height: 50px;
+    margin: 20px 0;
+    justify-content:center;
+    #start, #call, #hangup, #random{
+      width: 150px;
+      height:50px;
+      border-radius: 5px;
+      background-color: #33ADFF;
+      font-weight:bold;
+      font-size: 15px;
+      color: #fff;
+      display:flex;
+      align-items:center;
+      justify-content:center;
+      margin: 0 10px;
+      :disabled{
+        background-color: #cccccc;
+      }
+    }
+  }
+  
+`
+const mediaStreamConstraints = {
+  video: {
+    width: {
+      min: 640,
+      max: 1280
+    },
+    height: {
+      min: 480,
+      max: 720
+    }
+  },
+  audio: false
+}
+
+const offerOptions = {
+  offerToReceiveVideo: 1,
+}
+
+let localVideo;
+let remoteVideo;
+
+let callButton;
+let startButton;
+let hangupButton;
+let randomButton;
+
+let inputRoomId;
+
+let startTime = null;
+
+let localStream;
+let remoteStream;
+
+
+let remotePeerConnection;
+
+var fb = firebase.initializeApp({ 
+  apiKey: "AIzaSyB-q7FU6fQQTOShT_UbybVUBOcXHAhTILo",
+  authDomain: "vinpearl-2f7d4.firebaseapp.com",
+  databaseURL: "https://vinpearl-2f7d4.firebaseio.com",
+  projectId: "vinpearl-2f7d4",
+  storageBucket: "vinpearl-2f7d4.appspot.com",
+  messagingSenderId: "904958325116",
+  appId: "1:904958325116:web:e123ec52c8eea170"
+});
+const node = 'vinhome/videocall'
+var firebaseDB = fb.database();
+
+const servers = {iceServers: [
+  {'urls': 'stun:stun.l.google.com:19302'},
+  {'urls': 'stun:stun.services.mozilla.com'},
+]};
+var localPeerConnection = new RTCPeerConnection(servers);
+
+const id = uid(10)
+
+console.log(id)
+var partnerId = null
+var roomDetail = null
+
+const App = () => {
+  useEffect(() => {
+    localVideo = document.getElementById("local-video");
+    remoteVideo = document.getElementById("remote-video");
+    startButton = document.getElementById('start')  
+    // callButton = document.getElementById('call')
+    randomButton = document.getElementById('random')
+    hangupButton = document.getElementById('hangup');
+
+    inputRoomId = document.getElementById('input-id')
+  
+    // event
+    localPeerConnection.addEventListener('icecandidate', handleConnection);
+    localPeerConnection.addEventListener('addstream', gotRemoteMediaStream);
+
+    return () => {
+      
+    }
+  }, [])
+
+  const [isStart, setIsStart] = useState('');
+  const [isShowStartBtn, setIsShowStartBtn] = useState(false)
+  const [isShowRandomBtn, setIsShowRandomBtn] = useState(true)
+
+  const handleConnection = (e) => {
+    if(e.candidate)
+      sendMessage(JSON.stringify({'ice': e.candidate}))
+    else 
+      console.log("Sent All Ice") 
+  }
+
+  const gotRemoteMediaStream = (event) => {
+    const mediaStream = event.stream
+    console.log(mediaStream)
+    localVideo.style.zIndex = 2
+    localVideo.style.width = '35%'
+    remoteVideo.srcObject = mediaStream
+    remoteStream = mediaStream
+  }
+    
+  const sendMessage = (data) => {
+    if(!roomDetail) return
+    firebaseDB.ref(node).child(roomDetail.roomId).set({ ...roomDetail, message: data })
+  }
+
+  const watchFirebaseChange = (roomId) => {
+    firebaseDB.ref(node).child(roomId).on('value',(snapshot)=>{
+      let val = snapshot.val()
+      console.log(val)
+      readMessage(val)
+    })
+  }
+
+  const checkExistedRoomId = (roomId) => {
+    return new Promise(promise => {
+      firebaseDB.ref(node).child(roomId).once('value',(snapshot)=>{
+        console.log(snapshot.exists())
+        promise(snapshot.exists())
+      })
+    })
+  }
+
+  const getDetailRoom = (roomId) => {
+    return new Promise(promise => {
+      firebaseDB.ref(node).child(roomId).once('value',(snapshot)=>{
+        console.log(snapshot.val())
+        promise(snapshot.val())
+      })
+    })
+  }
+
+  const createRoom = (roomId) => {
+    roomDetail = {
+      ownerId : id,
+      senderId : '',
+      roomId,
+      message: ''
+    }
+    firebaseDB.ref(node).child(roomId).set(roomDetail)
+  }
+
+  const removeRoom = (roomId) => {
+    firebaseDB.ref(node).child(roomId).remove()
+  }
+
+  const leaveRoom = (roomId) => {
+    // firebaseDB.ref(node).child(roomId).update({
+    // })
+  }
+    
+  const readMessage = (data) => {
+    if(!data) return
+    var msg = JSON.parse(data.message);
+    var sender = data.senderId;
+    if (sender !== id) {
+      if (msg.ice !== undefined) {
+        localPeerConnection.addIceCandidate(new RTCIceCandidate(msg.ice));
+      }
+      else if (msg.sdp.type === "offer") {
+        // var r = confirm("Answer call?");
+        // if (r == true) {
+        console.log('read message offer')
+        localPeerConnection.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+          .then(() => localPeerConnection.createAnswer())
+          .then(answer => localPeerConnection.setLocalDescription(answer))
+          .then(() => sendMessage(JSON.stringify({'sdp': localPeerConnection.localDescription})))
+          .catch(error => {
+            console.error( error);
+          });
+        // } else {
+        //   alert("Rejected the call");
+        // }
+      }
+      else if (msg.sdp.type === "answer") {
+        console.log('read message answer')
+        console.log(localPeerConnection.localDescription)
+        localPeerConnection.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+        .catch(error => {
+          console.error( error);
+        });
+      }
+    }
+  };
+
+  const startAction = async () => {
+    setIsShowStartBtn(false)
+    setIsShowRandomBtn(false)
+    if(!inputRoomId) return
+    let rID = inputRoomId.value
+    let isExisted = await checkExistedRoomId(rID)
+    if(!isExisted){
+      createRoom(rID)
+    }
+    else{
+      console.log('roomId is existed')
+      roomDetail = await getDetailRoom(rID)
+      roomDetail.senderId = id;
+      if(roomDetail){
+        showFriendsFace()
+      }
+    }
+    setIsStart(true)
+    showMyFace()
+    watchFirebaseChange(rID)
+  }
+
+  const callAction = () => {
+    callButton.disabled = true
+    showFriendsFace()
+  }
+
+  const hangupAction = () => {
+    if(!roomDetail) return
+    if(roomDetail.ownerId === id){
+      removeRoom(roomDetail.roomId)
+    }
+    else{
+      leaveRoom(roomDetail.roomId)
+    }
+  }
+
+  const randomAction = () => {
+    if(!inputRoomId) return 
+    let randomId = Math.floor(Math.random()*1000000000)
+    inputRoomId.value = randomId
+    _onChangeRoomId()
+  }
+
+  const showMyFace = () => {
+    navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+    .then(stream => localVideo.srcObject = stream)
+    .then(stream => localPeerConnection.addStream(stream))
+    .catch(error => {
+      console.error('Error accessing media devices.', error);
+    });
+  }
+
+  const showFriendsFace = () => {
+    localPeerConnection.createOffer()
+    .then(offer => localPeerConnection.setLocalDescription(offer))
+    .then(() => sendMessage(JSON.stringify({'sdp': localPeerConnection.localDescription})) );
+  }
+
+  const _onChangeRoomId = (e) => {
+    let val = inputRoomId.value
+    if(val && val.length > 0){
+      setIsShowStartBtn(true)
+    }
+    else{
+      setIsShowStartBtn(false)
+    }
+  }
+
+  return (
+    <Bound>
+      <p>Your ID: {id}</p>
+      {
+        isStart?
+        <p>Room ID: {roomDetail && roomDetail.roomId}</p>
+        :
+        <input type='text' placeholder="input room ID" id='input-id' 
+          style={{width: '250px', height: '30px'}}
+          onChange={_onChangeRoomId}/>
+      }
+      
+      <div className='button-container'>
+        {
+          isStart ?
+          // <button id="call" onClick={()=>callAction()} disabled = {isShowCallBtn?false:true}>Call</button>
+          <button id="hangup" onClick={()=>hangupAction()}>Hang Up</button>
+          :
+          <React.Fragment>
+            <button id="start" onClick={()=>startAction()} disabled={isShowStartBtn?false:true} >Start</button>
+            <button id="random" onClick={()=>randomAction()} disabled={isShowRandomBtn?false:true} >Random</button>
+          </React.Fragment>
+        }
+          
+      </div>
+      <div className='video-container'>
+          <video id="local-video" autoPlay></video>
+          <video id="remote-video" autoPlay></video>
+      </div>
+    </Bound>
+  );
+}
+
+export default App;
