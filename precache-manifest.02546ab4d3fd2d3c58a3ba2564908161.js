69a34c52c10fc8c22f51e8a71b793b2dfc6ba457
Trinh Phu <phutrinh@Trinhs-MacBook-Pro.local>
2020-07-20 10:33:47 +0700

    Updates

---
+++ precache-manifest.02546ab4d3fd2d3c58a3ba2564908161.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0db966c51356ccaec89a255dd6a158",
+    "url": "/web-rtc-demo/index.html"
+  },
+  {
+    "revision": "dd6db4a2c3e2e0a3d722",
+    "url": "/web-rtc-demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ece79fa7eaf02d90d632",
+    "url": "/web-rtc-demo/static/js/2.8fef163a.chunk.js"
+  },
+  {
+    "revision": "a891348b5b506574d0decae32dbe4cfe",
+    "url": "/web-rtc-demo/static/js/2.8fef163a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6db4a2c3e2e0a3d722",
+    "url": "/web-rtc-demo/static/js/main.d48f5a95.chunk.js"
+  },
+  {
+    "revision": "40d011ea32a532f2b3d9",
+    "url": "/web-rtc-demo/static/js/runtime-main.8d5fed73.js"
+  }
+]);
